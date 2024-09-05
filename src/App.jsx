5d82eb709b5ca5aef7a52274009b51a08e2cb5db
dev-shahed <shahedthedev@gmail.com>
2024-09-06 00:40:28 +0600

    auth form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Auth from './components/Auth';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
 
@@ -21,8 +22,9 @@ function App() {
   }, []);
 
   return (
-    <Fragment>
+    <main className="container mx-auto mt-10 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-4">Blogs</h1>
+      <Auth />
       {isLoading ? (
         <p className="text-gray-500">Loading...</p>
       ) : blogs.length > 0 ? (
@@ -30,7 +32,7 @@ function App() {
       ) : (
         <p className="text-gray-500">No blogs found.</p>
       )}
-    </Fragment>
+    </main>
   );
 }
 
